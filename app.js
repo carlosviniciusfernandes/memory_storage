ecd5d848225fc42784f225497109d95a5f0581b1
Carlos Vinicius Fernandes <carlos.vinicius.fernandes@gmail.com>
2022-09-01 10:09:37 -0300

    Refactored the stack storageto a provider object decoupled from the controller

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { stackRouter, storeRouter } = require('./routes.js')
+const { stackController, storeController } = require('./controllers.js')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json())
+
+app.use('/stack', stackRouter);
+app.use('/store', storeRouter);
+
+app.clearStore = () => storeController.store = {}
+app.addToStore = (item) => storeController.store[item.key] = item.value
+app.setStoreItemTimeout = (key, time) => storeController.setTTL(key, time)
+
+module.exports = app
---
+++ controllers.js
@@ -0,0 +1,128 @@
+const inMemoryStorage = require('./providers.js')
+
+class StackController {
+    constructor(storage) {
+        this.storage = storage
+    }
+
+    isItemValid(item) {
+        return !(item == null)
+    }
+
+    addToStack(req, res) {
+        const item = req.body.item
+        if (this.isItemValid(item)) {
+            this.storage.pushToStack(item)
+            return res.status(200).json({
+                "message": `item has been added to the stack pile`,
+                "stackSize": this.storage.getStackSize(),
+                "item": item
+            })
+        } else {
+            return res.status(400).json({
+                "message": `A valid value for 'item' must be provided`,
+            })
+        }
+    }
+
+    getFromStack(req, res) {
+        if (this.storage.getStackSize() > 0) {
+            const item = this.storage.popFromStack()
+            return res.status(200).json({
+                "message": `item has been removed from the stack pile`,
+                "stackSize": this.storage.getStackSize(),
+                "item": item
+            })
+        } else {
+            return res.status(400).json({
+                "message": `Empty stack, could not retrieve an item from it`,
+            })
+        }
+    }
+
+}
+
+class StoreController {
+    constructor(storage) {
+        this.storage = storage
+        this.valueStore = {}
+        this.ttlStore = {}
+    }
+
+    checkTTL(key, ttlStore, valueStore) {
+        ttlStore[key] = ttlStore[key] - 1
+        if (ttlStore[key] <= 0) {
+            delete valueStore[key]
+            delete ttlStore[key]
+        }
+    }
+
+    setTTL(key, ttl) {
+        this.ttlStore[key] = ttl
+        setInterval(this.checkTTL, 1000, key, this.ttlStore, this.valueStore);
+    }
+
+    isNumeric(num) {
+        return !isNaN(num)
+    }
+
+    addToStore(req, res){
+        const key = req.body.key
+        const value = req.body.value
+        const ttl = req.body.ttl
+
+        if (key == null || !typeof key == 'string') {
+            return res.status(400).json({"message": `Validation error. Make sure 'key' it is a valid string`})
+        }
+
+        if (value === undefined) {
+            return res.status(400).json({"message": `Validation error. Make sure that 'value' is not undefined`})
+        }
+
+        if (ttl !== undefined) {
+            if (ttl && this.isNumeric(ttl)) {
+                this.setTTL(key, ttl)
+            } else {
+                return res.status(400).json({"message": `Validation error. TTL must be a numerical value`})
+            }
+        }
+
+        this.valueStore[key] = value
+        let message = `key-value pair {${key}: ${value}} has been set into the store`
+        if (ttl) message += ` with a time to live of ${ttl} seconds`
+
+        return res.status(200).json({"message": message})
+    }
+
+    getFromStore(req, res){
+        const key = req.params.key
+        const value = this.valueStore[key]
+        if (value !== undefined){
+            return res.status(200).json({ "value": `${value}` })
+        } else {
+            return res.status(404).json()
+        }
+    }
+
+    deleteFromStore(req, res){
+        const key = req.params.key
+        const value = req.body.value
+        if (this.valueStore[key] !== undefined){
+            delete this.valueStore[key]
+            delete this.ttlStore[key]
+            return res.json(200, {
+                "message": `key-value pair {${key}: ${value}} has been unset from the store`,
+            })
+        } else {
+            return res.status(404).json()
+        }
+    }
+}
+
+const stackController = new StackController(inMemoryStorage)
+const storeController = new StoreController(inMemoryStorage)
+
+module.exports = {
+    stackController,
+    storeController
+}
